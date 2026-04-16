d480501cf055a59e6d77c13fcc7faebc61532b19
Lindo0ta <mary.maestre@uniminuto.edu.co>
2026-04-16 08:19:46 -0500

    Fix: Deploy and DB

--- main.py
+++ main.py
@@ -18,11 +18,7 @@ DB_NAME     = os.getenv("DB_NAME", "postgres")
 API_KEY_SECRETA = os.getenv("API_KEY_SECRET")
 
 if not all([DB_HOST, DB_USER, DB_PASS, API_KEY_SECRETA]):
-    print(f"DB_HOST={os.getenv('DB_HOST')}")
-    print(f"DB_USER={os.getenv('DB_USER')}")
-    print(f"DB_PASS={'***' if os.getenv('DB_PASS') else 'NONE'}")
-    print(f"API_KEY_SECRET={os.getenv('API_KEY_SECRET')}")
-    # raise ValueError("Faltan variables de entorno criticas")
+     raise ValueError("Faltan variables de entorno criticas")
 
 async def verificar_api_key(x_api_key: str = Header(..., alias="X-API-Key")):
     if x_api_key != API_KEY_SECRETA:
@@ -34,7 +30,7 @@ app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 ORIGENES_PERMITIDOS = [
     "http://localhost:5173",
     "http://localhost:4173",
-    "https://calm-pebble-084048910.1.azurestaticapps.net",
+    "https://ficha-cu.vercel.app",
 ]
 
 app.add_middleware(
